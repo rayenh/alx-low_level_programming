8512d8185d9f432c806a9bf7ff888e5f75cf5e4c
rayenh <rayenhmissii@gmail.com>
2023-06-29 16:59:17 +0100

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,16 +13,16 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	while (dest[i] != '\0')
-	{
-		i++;
-	}
+		{
+			i++;
+		}
 	j = 0;
 	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
+		}
 
 	dest[i] = '\0';
 	return (dest);
