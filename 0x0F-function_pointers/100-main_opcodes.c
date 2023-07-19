d443c8093b7ea9f9f68b039926afc7b4f92b4795
rayen hmissi <rayenhmissii@gmail.com>
2023-07-19 11:34:44 -0700

    done

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,42 +1,42 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- *  * main - prints its own opcodes
- *   * @argc: number of arguments
- *    * @argv: array of arguments
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-		int bytes, i;
-			char *arr;
+	int bytes, i;
+	char *arr;
 
-				if (argc != 2)
-						{
-									printf("Error\n");
-											exit(1);
-												}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-					bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-						if (bytes < 0)
-								{
-											printf("Error\n");
-													exit(2);
-														}
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-							arr = (char *)main;
+	arr = (char *)main;
 
-								for (i = 0; i < bytes; i++)
-										{
-													if (i == bytes - 1)
-																{
-																				printf("%02hhx\n", arr[i]);
-																							break;
-																									}
-															printf("%02hhx ", arr[i]);
-																}
-									return (0);
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
